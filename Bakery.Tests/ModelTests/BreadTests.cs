a4f4d9c978fc15a33e9857dc2cf548e73ee487fa
Erik Z <molagg92@gmail.com>
2023-05-07 13:48:57 -0700

    set up a starting point fo my bakery app.

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    // Tests will go here!
+  }
+}
---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    // Tests will go here!
+  }
+}
